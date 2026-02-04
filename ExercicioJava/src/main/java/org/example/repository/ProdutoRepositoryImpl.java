1e03be32a1acb44e4aab2ed29db7a7e634a3a168
Vinicim <vinicius_anacleto@estudante.sesisenai.org.br>
2026-02-03 21:54:20 -0300

    Finalização
    Finalização da atividade / revisão para prova

--- ExercicioJava/src/main/java/org/example/repository/ProdutoRepositoryImpl.java
+++ ExercicioJava/src/main/java/org/example/repository/ProdutoRepositoryImpl.java
@@ -3,9 +3,8 @@ package org.example.repository;
 import org.example.model.Produto;
 import org.example.util.ConexaoBanco;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProdutoRepositoryImpl implements ProdutoRepository{
@@ -15,26 +14,95 @@ public class ProdutoRepositoryImpl implements ProdutoRepository{
         String sql="INSERT INTO produto (nome, preco, quantidade, categoria) VALUES (?, ?, ?, ?)";
 
         try (Connection conn = ConexaoBanco.conectar();
-             PreparedStatement )
+             PreparedStatement stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getQuantidade());
+            stmt.setString(4, produto.getCategoria());
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            if(rs.next()) {
+                produto.setId(rs.getInt(1));
+            }
+        }
+        return produto;
     }
 
     @Override
     public List<Produto> findAll() throws SQLException {
-        return List.of();
+        List<Produto> produtos = new ArrayList<>();
+        String sql = "SELECT * FROM produto";
+
+        try (Connection conn = ConexaoBanco.conectar();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                produtos.add(new Produto(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getDouble("preco"),
+                        rs.getInt("Quantidade"),
+                        rs.getString("categoria")
+                ));
+            }
+        }
+        return produtos;
     }
 
     @Override
     public Produto findById(int id) throws SQLException {
+        String sql = "SELECT * FROM produto WHERE id = ?";
+
+        try (Connection conn = ConexaoBanco.conectar();
+        PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new Produto(
+                            rs.getInt("id"),
+                            rs.getString("nome"),
+                            rs.getDouble("preco"),
+                            rs.getInt("quantidade"),
+                            rs.getString("categoria")
+                    );
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public Produto update(Produto produto) throws SQLException {
-        return null;
+        String sql = "UPDATE produto SET nome = ?, preco = ?, quantidade = ?, categoria = ? WHERE id = ?";
+
+        try (Connection conn = ConexaoBanco.conectar();
+        PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getQuantidade());
+            stmt.setString(4, produto.getCategoria());
+            stmt.setInt(5, produto.getId());
+            stmt.executeUpdate();
+
+        }
+        return produto;
     }
 
     @Override
     public void deleteById(int id) throws SQLException {
+    String sql = "DELETE FROM produto WHERE id = ?";
 
+    try (Connection conn =  ConexaoBanco.conectar();
+    PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+        stmt.setInt(1, id);
+        stmt.executeUpdate();
     }
+  }
 }
--- ExercicioJava/src/main/java/org/example/service/ProdutoServiceImpl.java
+++ ExercicioJava/src/main/java/org/example/service/ProdutoServiceImpl.java
@@ -1,34 +1,45 @@
 package org.example.service;
 
 import org.example.model.Produto;
-
+import org.example.repository.ProdutoRepositoryImpl;
+import org.example.repository.ProdutoRepository;
 import java.sql.SQLException;
 import java.util.List;
 
 public class ProdutoServiceImpl implements ProdutoService{
 
+    private final ProdutoRepository repository = new ProdutoRepositoryImpl();
+
     @Override
     public Produto cadastrarProduto(Produto produto) throws SQLException {
-        return null;
+        if (produto.getPreco() < 0) {
+            throw new IllegalArgumentException("Preco deve ser positivo.");
+        }
+        return repository.save(produto);
     }
 
     @Override
     public List<Produto> listarProdutos() throws SQLException {
-        return List.of();
+        return repository.findAll();
     }
 
     @Override
     public Produto buscarPorId(int id) throws SQLException {
-        return null;
+        return repository.findById(id);
     }
 
     @Override
     public Produto atualizarProduto(Produto produto, int id) throws SQLException {
-        return null;
+        produto.setId(id);
+        return repository.update(produto);
     }
 
     @Override
     public boolean excluirProduto(int id) throws SQLException {
+        if (repository.findById(id) != null) {
+            repository.findById(id);
+            return true;
+        }
         return false;
     }
 }
